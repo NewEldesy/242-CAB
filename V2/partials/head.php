622f2bfd09ee13c1b395c02f0602b977c4a5d7d7
*Eldesy <*New_Eldesy@hotmail.com*>
2024-02-20 19:24:13 +0000

    20/02/24

--- V2/partials/head.php
+++ V2/partials/head.php
@@ -43,47 +43,47 @@
                     <h3 class="text-primary">242 CAB</h3>
                 </a>
                 <div class="navbar-nav w-100">
-                    <a href="index.php?page=dashboard" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+                    <a href="index.php?page=dashboard" class="nav-item nav-link <?php if($_GET['page'] == "dashboard"){echo 'active';}?>"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-taxi me-2"></i>Taxi</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="index.php?page=taxi&action=view" class="dropdown-item">View</a>
-                            <a href="index.php?page=taxi&action=add" class="dropdown-item">Add</a>
+                        <a href="#" class="nav-link dropdown-toggle <?php if($_GET['page'] == "taxi"){echo 'active';}?>" data-bs-toggle="dropdown"><i class="fas fa-taxi me-2"></i>Taxi</a>
+                        <div class="dropdown-menu bg-transparent border-0  <?php if($_GET['page'] == "taxi"){echo 'show';}?>">
+                            <a href="index.php?page=taxi&action=view" class="dropdown-item <?php if($_GET['page'] == "taxi" && $_GET['action'] == "view"){echo 'active';}?>">View</a>
+                            <a href="index.php?page=taxi&action=add" class="dropdown-item <?php if($_GET['page'] == "taxi" && $_GET['action'] == "add"){echo 'active';}?>">Add</a>
                         </div>
                     </div>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-user-slash me-2"></i>Chauffeur</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="index.php?page=chauffeur&action=view" class="dropdown-item">View</a>
-                            <a href="index.php?page=chauffeur&action=add" class="dropdown-item">Add</a>
+                        <a href="#" class="nav-link dropdown-toggle <?php if($_GET['page'] == "chauffeur"){echo 'active';}?>" data-bs-toggle="dropdown"><i class="fas fa-user-slash me-2"></i>Chauffeur</a>
+                        <div class="dropdown-menu bg-transparent border-0  <?php if($_GET['page'] == "chauffeur"){echo 'show';}?>">
+                            <a href="index.php?page=chauffeur&action=view" class="dropdown-item <?php if($_GET['page'] == "chauffeur" && $_GET['action'] == "view"){echo 'active';}?>">View</a>
+                            <a href="index.php?page=chauffeur&action=add" class="dropdown-item <?php if($_GET['page'] == "chauffeur" && $_GET['action'] == "add"){echo 'active';}?>">Add</a>
                         </div>
                     </div>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-users-cog me-2"></i>Attribution</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="index.php?page=attribution&action=view" class="dropdown-item">View</a>
-                            <a href="index.php?page=attribution&action=add" class="dropdown-item">Add</a>
+                        <a href="#" class="nav-link dropdown-toggle <?php if($_GET['page'] == "attribution"){echo 'active';}?>" data-bs-toggle="dropdown"><i class="fas fa-users-cog me-2"></i>Attribution</a>
+                        <div class="dropdown-menu bg-transparent border-0  <?php if($_GET['page'] == "attribution"){echo 'show';}?>">
+                            <a href="index.php?page=attribution&action=view" class="dropdown-item <?php if($_GET['page'] == "attribution" && $_GET['action'] == "view"){echo 'active';}?>">View</a>
+                            <a href="index.php?page=attribution&action=add" class="dropdown-item <?php if($_GET['page'] == "attribution" && $_GET['action'] == "add"){echo 'active';}?>">Add</a>
                         </div>
                     </div>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-receipt me-2"></i>Versement</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="index.php?page=versement&action=view" class="dropdown-item">View</a>
-                            <a href="index.php?page=versement&action=add" class="dropdown-item">Add</a>
+                        <a href="#" class="nav-link dropdown-toggle <?php if($_GET['page'] == "versement"){echo 'active';}?>" data-bs-toggle="dropdown"><i class="fas fa-receipt me-2"></i>Versement</a>
+                        <div class="dropdown-menu bg-transparent border-0  <?php if($_GET['page'] == "versement"){echo 'show';}?>">
+                            <a href="index.php?page=versement&action=view" class="dropdown-item <?php if($_GET['page'] == "versement" && $_GET['action'] == "view"){echo 'active';}?>">View</a>
+                            <a href="index.php?page=versement&action=add" class="dropdown-item <?php if($_GET['page'] == "versement" && $_GET['action'] == "add"){echo 'active';}?>">Add</a>
                         </div>
                     </div>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-wrench me-2"></i>Panne</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="index.php?page=panne&action=view" class="dropdown-item">View</a>
-                            <a href="index.php?page=panne&action=add" class="dropdown-item">Add</a>
+                        <a href="#" class="nav-link dropdown-toggle <?php if($_GET['page'] == "panne"){echo 'active';}?>" data-bs-toggle="dropdown"><i class="fas fa-wrench me-2"></i>Panne</a>
+                        <div class="dropdown-menu bg-transparent border-0  <?php if($_GET['page'] == "panne"){echo 'show';}?>">
+                            <a href="index.php?page=panne&action=view <?php if($_GET['page'] == "panne" && $_GET['action'] == "view"){echo 'active';}?>" class="dropdown-item">View</a>
+                            <a href="index.php?page=panne&action=add <?php if($_GET['page'] == "panne" && $_GET['action'] == "add"){echo 'active';}?>" class="dropdown-item">Add</a>
                         </div>
                     </div>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-user me-2"></i>User</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="index.php?page=user&action=view" class="dropdown-item">View</a>
-                            <a href="index.php?page=user&action=add" class="dropdown-item">Add</a>
+                        <a href="#" class="nav-link dropdown-toggle <?php if($_GET['page'] == "user"){echo 'active';}?>" data-bs-toggle="dropdown"><i class="fa fa-user me-2"></i>User</a>
+                        <div class="dropdown-menu bg-transparent border-0  <?php if($_GET['page'] == "user"){echo 'show';}?>">
+                            <a href="index.php?page=user&action=view" class="dropdown-item <?php if($_GET['page'] == "user" && $_GET['action'] == "view"){echo 'active';}?>">View</a>
+                            <a href="index.php?page=user&action=add" class="dropdown-item <?php if($_GET['page'] == "user" && $_GET['action'] == "add"){echo 'active';}?>">Add</a>
                         </div>
                     </div>
                 </div>
@@ -97,7 +97,7 @@
             <!-- Navbar Start -->
             <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
                 <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
-                    <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
+                    <h2 class="text-primary mb-0"><i class="fas fa-car-alt"></i></h2>
                 </a>
                 <a href="#" class="sidebar-toggler flex-shrink-0">
                     <i class="fa fa-bars"></i>
@@ -109,7 +109,6 @@
                             <span class="d-none d-lg-inline-flex"><?= $_SESSION["Nom"]." ".$_SESSION["Prenom"];?></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item">Profile</a>
                             <a href="index.php?page=logout" class="dropdown-item">Log Out</a>
                         </div>
                     </div>

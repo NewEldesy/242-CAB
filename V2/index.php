eac7d1dc09f3c674746073a89908b7df101d6f5f
*Eldesy <*New_Eldesy@hotmail.com*>
2024-05-16 20:53:25 +0000

    MAJ index.php

--- V2/index.php
+++ V2/index.php
@@ -3,10 +3,14 @@
     include_once('model.php');
 
     if (!isset($_SESSION['UserID'])) {
+        //Si $_Post existe et si il n'est pas vide
         if (isset($_POST) && !empty($_POST)) {
             try {
+                //Fonction pour essayer de se connecter
                 $user = tryLogin($_POST);
+                //Si l'utilisateur existe
                 if (!empty($user)) {
+                    //Initialisation des session
                     $_SESSION["UserID"] = $user['UserID'];
                     $_SESSION["Email"] = $user['Email'];
                     $_SESSION["Nom"] = $user['Nom'];
@@ -19,18 +23,23 @@
                 handleDatabaseError($e->getMessage());
             }
         } else {
+            //Redirection
             include_once('app/signin.php');
         }
     } else {
         if (isset($_GET['page'])) {
+            //Initialisation variable page
             $page = $_GET['page'];
 
             switch ($page) {
                 case 'dashboard':
-                    // ... (fonctionnalités du tableau de bord)
+                    //Compte Taxis
                     $nTaxis = CompteTaxis();
+                    //Compte chauffeurs
                     $nChauffeurs = CompteChauffeurs();
+                    //Compte Pannes
                     $nPannes = ComptePannes();
+                    //Compte Versements
                     $nVersements = CompteVersements();
 
                     $lastV = getLastVersements();
@@ -40,13 +49,16 @@
                     break;
 
                 case 'logout':
+                    //Destruction session
                     session_destroy();
+                    //redirection
                     header('location: index.php');
                     break;
 
                 case 'taxi':
                     // ... (gestion des taxis)
                     if(isset($_GET['action'])) {
+                        //Initialisation variable action
                         $action = $_GET['action'];
                         
                         // Validez et filtrez le paramètre action pour éviter des problèmes de sécurité
@@ -54,45 +66,60 @@
                     
                         // Gestion de l'ajout de taxi
                         if($action == "add") {
+                            // Si $_Post existe et si $_Post n'est pas vide
                             if(isset($_POST) && !empty($_POST)) {
+                                //Ajout taxi
                                 addTaxi($_POST);
+                                //Redirection
                                 header('location:index.php?page=taxi&action=view');
                             }
                             else {
+                                //Redirection
                                 include_once('app/taxi_add.php');
                             }
                         }
                         // Gestion de la mise à jour de taxi
                         elseif($action == "update") {
+                            // Si id, $_Post existent et si $_Post n'est pas vide
                             if(isset($_POST) && !empty($_POST) && isset($_GET['id'])) {
+                                //Update Taxi by
                                 updateTaxi($_POST);
+                                //Redirection
                                 header('location:index.php?page=taxi&action=view');
                             }
                             else {
+                                //Si id existe
                                 if(isset($_GET['id'])) {
+                                    //Récupération taxi by id
                                     $result = getTaxiById($_GET['id']);
+                                    //Redirection
                                     include('app/taxi_update.php');
                                 } else {
                                     // Gérer l'absence de l'identifiant dans la requête
-                                    echo "Identifiant d'utilisateur manquant.";
+                                    echo "Identifiant manquant.";
                                     exit;
                                 }
                             }
                         }
                         // Gestion de la vue des taxis
                         elseif($action == "view") {
+                            //Récupération Taxi
                             $results = getTaxi();
+                            //Redirection
                             include_once('app/taxi_view.php');
                         }
                         // Gestion de la suppression de taxi
                         elseif($action == "delete") {
+                            //Si id existe
                             if(isset($_GET['id'])) {
+                                //Suppression Taxi par id
                                 removeTaxi($_GET['id']);
+                                //Redirection
                                 header('location: index.php?page=taxi&action=view');
                                 exit;
                             } else {
                                 // Gérer l'absence de l'identifiant dans la requête
-                                echo "Identifiant d'utilisateur manquant.";
+                                echo "Identifiant manquant.";
                                 exit;
                             }
                         }
@@ -102,6 +129,7 @@
                 case 'chauffeur':
                     // ... (gestion des chauffeurs)
                     if(isset($_GET['action'])) {
+                        //Initialisation variable action
                         $action = $_GET['action'];
                             
                         // Validez et filtrez le paramètre action pour éviter des problèmes de sécurité
@@ -109,46 +137,60 @@
                     
                         // Gestion de l'ajout de chauffeur
                         if($action == "add") {
+                            // Si $_Post existe et si $_Post n'est pas vide
                             if(isset($_POST) && !empty($_POST)) {
+                                //Ajout Chauffeur
                                 addChauffeur($_POST);
+                                //Redirection
                                 header('location:index.php?page=chauffeur&action=view');
                             }
                             else {
+                                //Récupération Taxis
                                 $taxis = getTaxi();
+                                //Redirection
                                 include_once('app/chauffeur_add.php');
                             }
                         }
                         // Gestion de la mise à jour de chauffeur
                         elseif($action == "update") {
+                            // Si id, $_Post existent et si $_Post n'est pas vide
                             if(isset($_POST) && !empty($_POST) && isset($_GET['id'])) {
+                                //Update chauffeur by id
                                 updateChauffeur($_POST);
+                                //Redirection
                                 header('location:index.php?page=chauffeur&action=view');
                             }
                             else {
                                 if(isset($_GET['id'])) {
+                                    //Recupération chauffeur par id
                                     $result = getChauffeurById($_GET['id']);
+                                    //Redirection
                                     include('app/chauffeur_update.php');
                                 } else {
                                     // Gérer l'absence de l'identifiant dans la requête
-                                    echo "Identifiant d'utilisateur manquant.";
+                                    echo "Identifiant manquant.";
                                     exit;
                                 }
                             }
                         }
                         // Gestion de la vue des chauffeurs
                         elseif($action == "view") {
+                            //Récupération chauffeur
                             $results = getChauffeur();
+                            //Redirection
                             include_once('app/chauffeur_view.php');
                         }
                         // Gestion de la suppression de chauffeur
                         elseif($action == "delete") {
                             if(isset($_GET['id'])) {
+                                //Suppression Chauffeur by id
                                 removeChauffeur($_GET['id']);
+                                //Redirection
                                 header('location: index.php?page=chauffeur&action=view');
                                 exit;
                             } else {
                                 // Gérer l'absence de l'identifiant dans la requête
-                                echo "Identifiant d'utilisateur manquant.";
+                                echo "Identifiant manquant.";
                                 exit;
                             }
                         }
@@ -158,6 +200,7 @@
                 case 'panne':
                     // ... (gestion des pannes)
                     if(isset($_GET['action'])) {
+                        //Initialisation variable action
                         $action = $_GET['action'];
                             
                         // Validez et filtrez le paramètre action pour éviter des problèmes de sécurité
@@ -165,46 +208,61 @@
                     
                         // Gestion de l'ajout de panne
                         if($action == "add") {
+                            // Si $_Post existe et si $_Post n'est pas vide
                             if(isset($_POST) && !empty($_POST)) {
+                                //Ajout Panne
                                 addPanne($_POST);
+                                //Redirection
                                 header('location:index.php?page=panne&action=view');
                             }
                             else {
+                                //Récupération taxis
                                 $taxis = getTaxi();
+                                //Redirection
                                 include_once('app/panne_add.php');
                             }
                         }
                         // Gestion de la mise à jour de panne
                         elseif($action == "update") {
+                            // Si id, $_Post existent et si $_Post n'est pas vide
                             if(isset($_POST) && !empty($_POST) && isset($_GET['id'])) {
+                                //Update panne par id
                                 updatePanne($_POST);
+                                //Redirection
                                 header('location:index.php?page=panne&action=view');
                             }
                             else {
                                 if(isset($_GET['id'])) {
+                                    //Récupération panne par id
                                     $result = getPanneById($_GET['id']);
+                                    //Redirection
                                     include('app/panne_update.php');
                                 } else {
                                     // Gérer l'absence de l'identifiant dans la requête
-                                    echo "Identifiant d'utilisateur manquant.";
+                                    echo "Identifiant manquant.";
                                     exit;
                                 }
                             }
                         }
                         // Gestion de la vue des pannes
                         elseif($action == "view") {
+                            //Récupération des pannes
                             $results = getPanne();
+                            //Redirection
                             include_once('app/panne_view.php');
                         }
                         // Gestion de la suppression de panne
                         elseif($action == "delete") {
+                            //Si identifiant existe
                             if(isset($_GET['id'])) {
+                                //Suppression panne par id
                                 removePanne($_GET['id']);
+                                //Redirection
                                 header('location: index.php?page=panne&action=view');
                                 exit;
                             } else {
                                 // Gérer l'absence de l'identifiant dans la requête
-                                echo "Identifiant d'utilisateur manquant.";
+                                echo "Identifiant manquant.";
                                 exit;
                             }
                         }
@@ -214,6 +272,7 @@
                 case 'versement':
                     // ... (gestion des versements)
                     if(isset($_GET['action'])) {
+                        //Initialisation de la variable action
                         $action = $_GET['action'];
                             
                         // Validez et filtrez le paramètre action pour éviter des problèmes de sécurité
@@ -221,8 +280,11 @@
                     
                         // Gestion de l'ajout de versement
                         if($action == "add") {
+                            //Si $_Post existe et s'il est vide
                             if(isset($_POST) && !empty($_POST)) {
+                                //Ajout versement
                                 addVersement($_POST);
+                                //Redirection après ajout
                                 header('location:index.php?page=versement&action=view');
                             }
                             else {
